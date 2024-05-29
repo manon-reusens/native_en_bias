39b235c324a307f0dc36257025d911ecf1634d35
Manon Reusens <u0146965@kuleuven.be>
2024-05-29 17:54:24 +0200

    add modes

--- gather_output_gpt.py
+++ gather_output_gpt.py
@@ -38,6 +38,14 @@ parser.add_argument(
                     type=str,
                     help="Give the full path of where you want to save the output file",
 )
+parser.add_argument(
+                    "--mode",
+                    action="store",
+                    type=str,
+                    default='standard',
+                    choices=['standard','add_all_native','add_all_non_native','guess_native','reformulate'],
+                    help="Give the full path of where you want to save the output file",
+)
 
 import openai
 import pandas as pd
@@ -48,90 +56,40 @@ import time
 
 def gather_answers(index,df,model='gpt-3.5-turbo'):
     temperature=temp_dict[df.loc[index]['dataset_id']]
+    #change the task definitions
     if df.loc[index]['dataset_id']==3:
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": df.loc[index]['task_def']+ ' Only respond with the rating.'},
-            {"role":'assistant',"content":'Understood'},
-            {"role": "user", "content": df.loc[index]['final_prompt_en']}
-            ],
-            temperature=temperature,
-            logprobs=True,
-            top_logprobs=5,
-            seed=42
-        )
+        task_def=df.loc[index]['task_def']+ ' Only respond with the rating.'
     elif df.loc[index]['dataset_id']==1:
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": df.loc[index]['task_def']+  ' Only respond with the predicted last sentence.'},
-            {"role":'assistant',"content":'Understood'},
-            {"role": "user", "content": df.loc[index]['final_prompt_en']}
-            ],
-            temperature=temperature,
-            logprobs=True,
-            top_logprobs=5,
-            seed=42
-        )
+        task_def=df.loc[index]['task_def']+  ' Only respond with the predicted last sentence.'
     elif df.loc[index]['dataset_id']==6:
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": df.loc[index]['task_def']+ ' Only respond with the news article.'},
-            {"role":'assistant',"content":'Understood'},
-            {"role": "user", "content": df.loc[index]['final_prompt_en']}
-            ],
-            temperature=temperature,
-            logprobs=True,
-            top_logprobs=5,
-            seed=42
-        )
+        task_def=df.loc[index]['task_def']+ ' Only respond with the news article.'
     elif df.loc[index]['dataset_id']==7:
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": df.loc[index]['task_def']+ '  Only respond with the paragraph.'},
-            {"role":'assistant',"content":'Understood'},
-            {"role": "user", "content": df.loc[index]['final_prompt_en']}
-            ],
-            temperature=temperature,
-            logprobs=True,
-            top_logprobs=5,
-            seed=42
-        )
+        task_def=df.loc[index]['task_def']+ '  Only respond with the paragraph.'
     elif df.loc[index]['dataset_id']==8:
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": df.loc[index]['task_def']+ ' Only respond with the paraphrased sentence.'},
-            {"role":'assistant',"content":'Understood'},
-            {"role": "user", "content": df.loc[index]['final_prompt_en']}
-            ],
-            temperature=temperature,
-            logprobs=True,
-            top_logprobs=5,
-            seed=42
-        )
+        task_def=df.loc[index]['task_def']+ ' Only respond with the paraphrased sentence.'
     else:
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": df.loc[index]['task_def']},
-            {"role":'assistant',"content":'Understood'},
-            {"role": "user", "content": df.loc[index]['final_prompt_en']}
-            ],
-            temperature=temperature,
-            logprobs=True,
-            top_logprobs=5,
-            seed=42
-        )
+        task_def=df.loc[index]['task_def']
+    
+    #change the system prompt
+    if args.mode=='add_all_native':
+        system_prompt="You are a helpful assistant.Respond as if you are interacting with a native English speaker"
+    elif args.mode=='add_all_non_native':
+        system_prompt="You are a helpful assistant.Respond as if you are interacting with a non-native English speaker"
+    else:
+        system_prompt="You are a helpful assistant."
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": task_def},
+        {"role":'assistant',"content":'Understood'},
+        {"role": "user", "content": df.loc[index]['final_prompt_en']}
+        ],
+        temperature=temperature,
+        logprobs=True,
+        top_logprobs=5,
+        seed=42
+    )
     return response
 
 if __name__ == "__main__":
@@ -150,8 +108,25 @@ if __name__ == "__main__":
     os.environ['OPENAI_API_KEY']=args.key
 
     client = OpenAI()
-    df_final_set[args.model+' replies']=np.NaN
-    df_final_set[args.model+' logprobs']=np.NaN
+
+    if args.mode=='add_all_native':
+        col_replies=args.model+' replies_all_native'
+        col_logprobs=args.model+' logprobs_all_native'
+    elif args.mode=='add_all_non_native':
+        col_replies=args.model+' replies_all_non_native'
+        col_logprobs=args.model+' logprobs_all_non_native'
+    elif args.mode=='standard':
+        col_replies=args.model+' replies'
+        col_logprobs=args.model+' logprobs'
+    elif args.mode=='guess_native':
+        col_replies=args.model+' replies_guess_native'
+        col_logprobs=args.mode+' logprobs_guess_native'
+    elif args.mode=='reformulate':
+        col_replies=args.model+' replies_reformulate'
+        col_logprobs=args.mode+' logprobs_reformulate'
+
+    df_final_set[col_replies]=None
+    df_final_set[col_logprobs]=None
 
     temp_dict={0:0,1:0.7,2:0,3:0,4:0,5:0,6:0.7,7:0.7,8:0.7,9:0}
 
@@ -167,8 +142,8 @@ if __name__ == "__main__":
             df_final_set.to_parquet(args.output_file)
         try:
             result=gather_answers(i,df_final_set, model=model)
-            df_final_set.at[i,args.model+' replies']=result.choices[0].message.content
-            df_final_set.at[i,args.model+' logprobs']=str(result.choices[0].logprobs.content)
+            df_final_set.at[i,col_replies]=result.choices[0].message.content
+            df_final_set.at[i,col_logprobs]=str(result.choices[0].logprobs.content)
             results_full.append(result)
             cleaned_reasults_gpt3.append(result.choices[0].message.content)
         except:
