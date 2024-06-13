166980d022180bd476cdcb82e2f42258c500dab4
Manon Reusens <u0146965@kuleuven.be>
2024-06-13 17:06:29 +0200

    make more flexible

--- gather_scores.py
+++ gather_scores.py
@@ -44,14 +44,12 @@ parser.add_argument(
 parser.add_argument(
                     "--generated_ground_truth",
                     action="store",
-                    type=str,
                     default=None,
                     help="add new ground truth file for the generative tasks."
 )
 
 def merge_datasets(df1,df_groundtruth):
-    df_groundtruth=df_groundtruth.rename(columns={df_groundtruth.columns[8]:'generated_req_output'})
-    print(df_groundtruth.columns)
+    df_groundtruth=df_groundtruth.rename(columns={args.column:'generated_req_output'})
     df_nec=df_groundtruth[['nat_instr_id','generated_req_output']]
     df_merged=pd.merge(df1,df_nec,on='nat_instr_id')
     return df_merged
@@ -61,9 +59,7 @@ if __name__ == "__main__":
     df=pd.read_parquet(args.input_file)
     if args.generated_ground_truth != None:
         df_new=pd.read_parquet(args.generated_ground_truth)
-        print('we are merging now')
         df=merge_datasets(df,df_new)
-    print(args.generated_ground_truth)
 
     if args.score=='all' or args.score=='accuracy':
         acc_run=Accuracy_Runner(df,args.column) 
@@ -87,24 +83,3 @@ if __name__ == "__main__":
 
 
     df.to_parquet(args.output_file)
-
-    # def gather_results_native_nonnative(self,dataset_id):
-    #     df_classresults_corr=self.df[(self.df['dataset_id_x']==dataset_id)& ((df_classresults['level_en']=='C1') | (df_classresults['level_en']=='C2')) & (df_classresults['user_id']>50)]
-    #     users_to_exclude=df_classresults_corr['user_id'].value_counts().loc[~df_classresults_corr['user_id'].value_counts().ge(10)] #note: 10 should be half of the available examples
-    #     for index in users_to_exclude.index:
-    #         df_classresults_corr=df_classresults_corr.loc[df_classresults_corr['user_id']!=index]
-        
-    #     results=df_classresults_corr.groupby('user_id')['accuracy_score_gpt3'].mean()
-    #     final=pd.merge(results,df_classresults[['user_id','nat_lang']],on='user_id',how='inner').drop_duplicates()
-
-    #     score_en=[]
-    #     score_non_en=[]
-    #     for i,row in final.iterrows():
-    #         if 'en' in row['nat_lang']:
-    #             score_en.append(row['accuracy_score_gpt3'])
-    #         else:
-    #             score_non_en.append(row['accuracy_score_gpt3'])
-    #     print('the overall score for EN for set 10 is ', sum(score_en)/len(score_en))
-    #     print('the overall score for non-EN for set 10 is ', sum(score_non_en)/len(score_non_en))
-        
-    #     return final, score_en, score_non_en
